4fa1c98ed9568481979ed5768977c63ca1925eee
HarpyEagle <mwerezak@gmail.com>
2018-05-10 12:43:33 -0400

    Move terrain tiles into WorldMap scene

--- scripts/HexCursor.gd
+++ scripts/HexCursor.gd
@@ -1,14 +1,14 @@
 extends Sprite
 
-onready var world = $"../WorldMap"
-onready var tiles = $"../WorldMap/TerrainTiles"
+onready var root = $".."
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
-		global_position = world.get_global_mouse_position()
+		global_position = root.world.get_global_mouse_position()
 		_snap_to_grid()
 
 func _snap_to_grid():
+	var tiles = root.world.terrain
 	var map_pos = tiles.world_to_map(global_position)
 	#print(tiles.get_cellv(map_pos))
 	global_position = tiles.map_to_world(map_pos)
