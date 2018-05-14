931641209fe615c3d783b17d44f4f13c05addaf4
HarpyEagle <mwerezak@gmail.com>
2018-05-14 14:40:31 -0400

    Spawn unit tool checks if the unit can be placed

--- scripts/GUI/ContextPanel/SpawnUnitTool.gd
+++ scripts/GUI/ContextPanel/SpawnUnitTool.gd
@@ -5,5 +5,8 @@ const Unit = preload("res://scripts/Units/Unit.tscn")
 func unit_cell_input(world_map, cell_pos, event):
 	if event.is_action_pressed("click_select"):
 		var spawn_unit = Unit.instance()
-		world_map.add_object(spawn_unit, cell_pos)
-		spawn_unit.facing = randi()
+		if world_map.unit_can_place(spawn_unit, cell_pos):
+			world_map.add_object(spawn_unit, cell_pos)
+			spawn_unit.facing = randi()
+		else:
+			spawn_unit.queue_free()
