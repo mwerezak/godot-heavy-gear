7f901b946f17b83ce94b34f602543aca84fc0e8e
HarpyEagle <mwerezak@gmail.com>
2018-05-15 23:44:34 -0400

    Unit type inheritance

--- scripts/game/UnitTypes.gd
+++ scripts/game/UnitTypes.gd
@@ -7,11 +7,34 @@ const Factions = preload("res://scripts/game/Factions.gd")
 const TYPE_VEHICLE = "vehicle"
 const TYPE_INFANTRY = "infantry"
 
-var INFO = {
+const DEFAULTS = {
+	default_gear = {
+		nato_symbol = "gear",
+		unit_type = TYPE_VEHICLE,
+		height = 1.0,
+		movement = [
+			{ mode = MovementModes.WALKER, speed = 5.0 },
+			{ mode = MovementModes.GROUND, speed = 6.0 },
+		],
+		action_points = 1,
+	},
+	default_infantry = {
+		nato_symbol = "infantry",
+		unit_type = TYPE_INFANTRY,
+		height = 0.5,
+		movement = [
+			{ mode = MovementModes.INFANTRY, speed = 3.0 },
+		],
+		action_points = 1,
+	},
+}
+
+const INFO = {
 	dummy_vehicle = {
 		name = "Dummy Vehicle",
 		short_desc = "Armored Personnel Carrier", #one-line description of this unit
 		nato_symbol = "wheeled_apc",
+		crew_rank = Factions.RANK_SPECIALIST,
 		
 		unit_type = TYPE_VEHICLE,
 		height = 1.0,
@@ -25,39 +48,52 @@ var INFO = {
 	dummy_infantry = {
 		name = "Dummy Infantry",
 		short_desc = "Infantry Squad",
-		nato_symbol = "infantry",
-		
-		unit_type = TYPE_INFANTRY,
-		height = 0.5,
-		movement = [
-			{ mode = MovementModes.INFANTRY, speed = 3.0 },
-		],
-		
-		action_points = 1,
+		crew_rank = Factions.RANK_SQUAD_LEAD,
+		inherits = "default_infantry",
 	},
 	dummy_gear = {
 		name = "Dummy Gear",
 		short_desc = "Battle Gear",
-		nato_symbol = "gear",
-		crew_rank = rank("enlisted", Factions.EN_SPECIALIST),
-		
-		unit_type = TYPE_VEHICLE,
-		
-		height = 1.0,
-		movement = [
-			{ mode = MovementModes.WALKER, speed = 5.0 },
-			{ mode = MovementModes.GROUND, speed = 6.0 },
-		],
-		
-		action_points = 1,
+		crew_rank = Factions.RANK_SPECIALIST,
+		inherits = "default_gear",
 	}
 }
 
 var _CACHE = {}
 
+func _apply_override(parent, child):
+	var result = parent.duplicate()
+	for key in child:
+		result[key] = child[key]
+	return result
+
+func _resolve_dependencies(child_id, resolve_cache):
+	if resolve_cache.has(child_id):
+		return resolve_cache[child_id]
+	
+	var child = INFO[child_id] if INFO.has(child_id) else DEFAULTS[child_id]
+	if child.has("inherits"):
+		var parent_id = child.inherits
+		if !resolve_cache.has(parent_id):
+			_resolve_dependencies(parent_id, resolve_cache)
+		
+		var parent = resolve_cache[parent_id]
+		child = _apply_override(parent, child)
+	
+	resolve_cache[child_id] = child
+
 func _init():
-	for model_id in INFO:
-		_CACHE[model_id] = UnitInfo.new(INFO[model_id])
+	var all_models = INFO.keys()
+	
+	## resolve all inherit references
+	var resolve_cache = {}
+	for model_id in all_models:
+		_resolve_dependencies(model_id, resolve_cache)
+	
+	for model_id in all_models:
+		var resolved_info = resolve_cache[model_id]
+		_CACHE[model_id] = UnitInfo.new(resolved_info)
 
 func get_info(model_id):
 	return _CACHE[model_id]
+
