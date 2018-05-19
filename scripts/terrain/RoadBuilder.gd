cb0412152d80966d9c0c48035c1ca3cd52c51ddd
HarpyEagle <mwerezak@gmail.com>
2018-05-19 19:23:22 -0400

    Fix road being extended in awkward ways

--- scripts/terrain/RoadBuilder.gd
+++ scripts/terrain/RoadBuilder.gd
@@ -34,15 +34,7 @@ func _get_neighbors(segment, cell_pos):
 		var next_pos = neighbors[next_dir]
 		if _placed_cells.has(next_pos):
 			var other = _placed_cells[next_pos]
-			var can_connect = false
-			if segment == null:
-				## segment is null, so we are looking for neighbors that can extend to the currently unoccupied cell_pos
-				can_connect = other.can_extend(next_pos, cell_pos)
-			else:
-				## segment isn't null, so we are looking for neighbors to merge or join.
-				can_connect = segment != other ## can't merge or join with ourselves
-				
-			if can_connect:
+			if segment == null || segment != other:
 				connections[next_pos] = other
 				conn_dir[next_pos] = HexUtils.reverse_dir(next_dir)
 	
@@ -62,16 +54,18 @@ func _generate_segments(road_map):
 		if !candidates.empty():
 			## connect to existing segment
 			var comparer = RoadConnectionComparer.new(cell_pos, candidates, candidates_dir)
-			var best = SortingUtils.get_min_item(candidates.keys(), comparer, "compare")
+			var best_pos = SortingUtils.get_min_item(candidates.keys(), comparer, "compare")
 			
-			var existing_segment = candidates[best]
-			existing_segment.extend(best, cell_pos)
-			_placed_cells[cell_pos] = existing_segment
-		else:
-			var new_segment = RoadSegment.instance()
-			new_segment.setup(world_map, cell_pos)
-			_segments.push_back(new_segment)
-			_placed_cells[cell_pos] = new_segment
+			var existing_segment = candidates[best_pos]
+			if existing_segment.can_extend(best_pos, cell_pos):
+				existing_segment.extend(best_pos, cell_pos)
+				_placed_cells[cell_pos] = existing_segment
+				continue
+		
+		var new_segment = RoadSegment.instance()
+		new_segment.setup(world_map, cell_pos)
+		_segments.push_back(new_segment)
+		_placed_cells[cell_pos] = new_segment
 
 func _merge_or_join_segments():
 	var merge_info = {}
@@ -140,15 +134,13 @@ class RoadConnectionComparer:
 			join_angle[next_pos] = min_angle
 	
 	func compare(pos_left, pos_right):
-		return SortingUtils.lexical_sort(
-			_conn_lexical(neighbors[pos_left], pos_left), _conn_lexical(neighbors[pos_right], pos_right)
-		)
+		return _conn_metric(neighbors[pos_left], pos_left) < _conn_metric(neighbors[pos_right], pos_right)
+#		return SortingUtils.lexical_sort(
+#			_conn_lexical(neighbors[pos_left], pos_left), _conn_lexical(neighbors[pos_right], pos_right)
+#		)
 	
-	func _conn_lexical(segment, next_pos):
-		if cell_pos == Vector2(27,15):
-			print(next_pos, ": ", abs(2 - segment.total_connections(next_pos)))
-			print(next_pos, ": ", join_angle[next_pos] if join_angle != null else 100)
-		return [
-			abs(2 - segment.total_connections(next_pos)), 
-			join_angle[next_pos] if join_angle != null else 100,
-		]
+	func _conn_metric(segment, next_pos):
+		var angle = join_angle[next_pos]
+		if angle != null:
+			return angle
+		return abs(2 - segment.total_connections(next_pos))
