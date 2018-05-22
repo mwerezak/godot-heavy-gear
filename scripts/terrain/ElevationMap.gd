07b69534b29d29bf77fdf35b6220be6a012858be
HarpyEagle <mwerezak@gmail.com>
2018-05-22 03:24:58 -0400

    Plane-based terrain height interpolation

--- scripts/terrain/ElevationMap.gd
+++ scripts/terrain/ElevationMap.gd
@@ -28,10 +28,10 @@ func load_hex_map(raw_elevation):
 		_update_bounds(_COL, hex_pos.x, hex_pos.y)
 		_update_bounds(_ROW, hex_pos.y, hex_pos.x)
 	
-	## precalculate bilinear coefficients
+	## precalculate plane coefficients
 	_coefficients.clear()
 	for hex_pos in _elevation_map:
-		_get_bilinear_coefficients(hex_pos)
+		_get_plane_coefficients(hex_pos)
 
 func _update_bounds(axis, index, value):
 	if _bounds[axis].has(index):
@@ -66,31 +66,38 @@ func _world_to_axial(world_pos):
 func _axial_to_world(axial_pos):
 	return terrain_grid.axial_to_world(axial_pos) + terrain_grid.cell_size/2.0
 
-func _get_bilinear_coefficients(origin_hex):
+func _get_plane_coefficients(origin_hex):
 	if _coefficients.has(origin_hex):
 		return _coefficients[origin_hex]
 	
-	var rval = _calc_bilinear_coefficients(origin_hex)
+	var rval = _calc_plane_coefficients(origin_hex)
 	_coefficients[origin_hex] = rval
 	return rval
 
-func _calc_bilinear_coefficients(origin_hex):
-	var trapezoid = [ origin_hex, HexUtils.get_step(origin_hex, 0), HexUtils.get_step(origin_hex, 2), HexUtils.get_step(origin_hex, 4) ]
-
+const _UPPER_TRIANGLE = 0
+const _LOWER_TRIANGLE = 1
+func _calc_plane_coefficients(origin_hex):
+	## each trapezoid consists of an upper triangle (x>y) and a lower triangle (y>x)
+	var trap_hexes = [ origin_hex, HexUtils.get_step(origin_hex, 0), HexUtils.get_step(origin_hex, 2), HexUtils.get_step(origin_hex, 4) ]
 	## get the elevation at each corner of the trapezoid
-	var z = [0, 0, 0, 0]
+	var trap_vec = [null, null, null, null]
 	for i in range(4):
-		z[i] = _get_hex_elevation(trapezoid[i])
+		var z = _get_hex_elevation(trap_hexes[i])
+		
+		if z == null:
+			return null
+		
+		var pos = world_map.get_terrain_pos(trap_hexes[i])
+		trap_vec[i] = Vector3(pos.x, pos.y, z)
 	
-	if z.has(null):
-		return
-	
-	var a = [0, 0, 0, 0]
-	a[0] = z[0]
-	a[1] = z[1] - z[0]
-	a[2] = z[3] - z[0]
-	a[3] = z[0] - z[1] - z[3] + z[2]
-	return a
+	return [
+		_get_plane(trap_vec[0], trap_vec[2], trap_vec[1]), #upper triangle
+		_get_plane(trap_vec[0], trap_vec[2], trap_vec[3]), #lower triangle
+	]
+
+func _get_plane(plane_origin, p1, p2):
+	var plane_normal = (p1 - plane_origin).cross(p2 - plane_origin)
+	return [ plane_normal.x, plane_normal.y, plane_normal.z, -plane_normal.dot(plane_origin) ]
 
 func get_elevation(world_pos):
 	## get the elevation trapezoid containing world_pos
@@ -98,12 +105,15 @@ func get_elevation(world_pos):
 	var trap_origin = axial_pos.floor()
 	var origin_hex = world_map.get_terrain_hex(_axial_to_world(trap_origin))
 	
-	## bilinear interpolation
-	var v = axial_pos - trap_origin
-	var a = _get_bilinear_coefficients(origin_hex)
-	if a == null:
+	var plane = _get_plane_coefficients(origin_hex)
+	if !plane:
 		return _get_hex_elevation(origin_hex)
-	return a[0] + a[1]*v.x + a[2]*v.y + a[3]*v.x*v.y
-	#var z_y0 = lerp(z[0], z[1], v.x)
-	#var z_y1 = lerp(z[3], z[2], v.x)
-	#return lerp(z_y0, z_y1, v.y)
+	
+	var v = axial_pos - trap_origin
+	if v.x > v.y:
+		plane = plane[_UPPER_TRIANGLE]
+	else:
+		plane = plane[_LOWER_TRIANGLE]
+	
+	return -(plane[0]*world_pos.x + plane[1]*world_pos.y + plane[3])/plane[2]
+
